efc4f7a35e7b10e34032e1af3157c61c0b2a5f4a
Merari Cisneros <meraridanielcisneros@gmail.com>
2022-07-07 20:24:03 -0700

    added static folder to serve U.S Map

--- finance_app/finance_app/urls.py
+++ finance_app/finance_app/urls.py
@@ -15,8 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include,path
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('map/', include('Wage_Map.urls'), name='map'),
     path('admin/', admin.site.urls),
+  
 ]
+
+urlpatterns += staticfiles_urlpatterns()
