8210adb98f839359c620f851a426b8d337456e1a
Merari Cisneros <meraridanielcisneros@gmail.com>
2022-07-09 10:55:55 -0700

    moved items into git ignore

--- finance_app/finance_app/settings.py
+++ finance_app/finance_app/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure--!^+82zg062mp)ut@7@h3$1pu)5r)v=b%h&p3u0liqz0a@2$mk'
+SECRET_KEY = config("SECRET_kEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
