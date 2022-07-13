1554c8b449a660512eea401ea2ba3e9ddb36aaf3
Merari Cisneros <meraridanielcisneros@gmail.com>
2022-07-12 21:29:01 -0700

    Overlayed states grid using statesData GeoJson

--- finance_app/Wage_Map/static/Wage_Map/map.js
+++ finance_app/Wage_Map/static/Wage_Map/map.js
@@ -1,7 +1,7 @@
 
-//import {statesData} from './us-states'
 
-//console.log(statesData)
+
+
 
 var map = L.map('wage_map').setView([37.80, -96], 4);
 
@@ -9,5 +9,10 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '© OpenStreetMap'
 }).addTo(map);
+
+var geojsonLayer = L.geoJSON(statesData);
+geojsonLayer.addTo(map);
+
+console.log(statesData)
 
 
