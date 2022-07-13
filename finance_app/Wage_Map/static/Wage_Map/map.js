aa1a0cb78a27f209326456b913bb3173a7363033
Merari Cisneros <meraridanielcisneros@gmail.com>
2022-07-12 20:52:02 -0700

    U.S GeoJson data is not found when loading to the DOM

--- finance_app/Wage_Map/static/Wage_Map/map.js
+++ finance_app/Wage_Map/static/Wage_Map/map.js
@@ -1,6 +1,13 @@
-var map = L.map('wage_map').setView([39.50, -98.35], 3.5);
+
+import {statesData} from './us-states'
+
+console.log(statesData)
+
+var map = L.map('wage_map').setView([37.80, -96], 4);
 
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '© OpenStreetMap'
 }).addTo(map);
+
+
